b72372531c78b430fc9f70da9e4eec1c916f2e50
Mrzarabi <m.hadi.z.1997@gmail.com>
2020-08-21 10:56:19 +0430

    add two routes resource for category and brand

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->namespace('Api\V1')->group( function () {
+    
+    Route::resource('category', 'CategoryController');
+    Route::resource('brnad', 'BrandController');
 });
