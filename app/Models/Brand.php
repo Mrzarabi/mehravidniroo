ae3c4a959ad248caa0e54903a49448ef0b3c0ca6
Mrzarabi <m.hadi.z.1997@gmail.com>
2020-08-21 05:25:55 +0430

    create model Brand

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -7,6 +7,26 @@ use Illuminate\Database\Eloquent\Model;
 class Brand extends Model
 {
     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'desc',
+        'image'
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    /**
      * Relations
      */
 
