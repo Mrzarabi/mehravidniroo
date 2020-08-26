8bd7b71aea2b126850ea7cfadcd0ccc8073c14d1
Mrzarabi <m.hadi.z.1997@gmail.com>
2020-08-26 13:16:55 +0430

    add some change in resource category

--- app/Http/Resources/Api/V1/Category/Category.php
+++ app/Http/Resources/Api/V1/Category/Category.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Api\V1\Category;
 
+use App\Http\Resources\Api\V1\Product\ProductCollection;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class Category extends JsonResource
@@ -15,10 +16,12 @@ class Category extends JsonResource
     public function toArray($request)
     {
         return [
+            'id'    => $this->id,
             'title' => $this->title,
             'desc' => $this->desc,
             'image' => $this->image,
-            // 'children' => new SubCategoryCollection($this->categories)
+            'children' => new SubCategoryCollection($this->categories),
+            'products' => new ProductCollection($this->products)
         ];
     }
 }
