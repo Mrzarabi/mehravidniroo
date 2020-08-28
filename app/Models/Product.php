cec8cdedb9448644f793155cc5bff86a1974ed77
Mrzarabi <m.hadi.z.1997@gmail.com>
2020-08-28 12:35:34 +0430

    add field images

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -14,7 +14,7 @@ class Product extends Model
      */
     protected $fillable = [
         'category_id',
-        // 'brand_id',
+        'images',
         'title',
         'desc',
         'body',
