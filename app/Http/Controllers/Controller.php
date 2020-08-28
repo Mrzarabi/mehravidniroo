e09a351b1357610e3acb4ffcb2cddd7869a4e827
Mrzarabi <m.hadi.z.1997@gmail.com>
2020-08-28 11:51:55 +0430

    delete extra method

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -12,23 +12,6 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-    // public function upload_image($image) {
-
-    //     $time = Carbon::now();   
-    //     $file_path = "uploads/{$time->year}/{$time->month}/{$time->day}";
-    //     $file_ext = $image->getClientOriginalExtension();
-    //     $file_name = rtrim($image->getClientOriginalName(), ".$file_ext");
-    //     $file_name = time() . '_' . substr($file_name, 0, 30);
-        
-    //     // Create directories if doesn't exists
-    //     if (!file_exists( public_path($file_path) )) {
-    //         mkdir(public_path($file_path), 0777, true);
-    //     }
-
-    //     $image->move( public_path("$file_path/$file_name.$file_ext") );
-    // }
-
-    
     public static function upload_image($image)
     {
         // Create file name & file path with /year/month/day/filename formats
