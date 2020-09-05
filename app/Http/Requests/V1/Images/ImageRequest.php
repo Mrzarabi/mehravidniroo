64e8a3c4b4348711339a78e86520bc4a0d844b5c
Mrzarabi <m.hadi.z.1997@gmail.com>
2020-09-05 16:55:57 +0430

    add request image

---
+++ app/Http/Requests/V1/Images/ImageRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests\V1\Images;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ImageRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'images'            => 'nullable',
+            'images.*'          => [ 'image', 'mimes:jpeg,jpg,png,gif' ],
+
+            /**
+             * Relations
+             */
+
+            'products.*' => 'required|integer|exists:products,id',
+        ];
+    }
+}
