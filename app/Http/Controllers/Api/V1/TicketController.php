f80cf1e8dbe96f71e63aa7ce3586502800d4b338
Mrzarabi <m.hadi.z.1997@gmail.com>
2020-09-05 16:55:28 +0430

    add controler ticket

--- app/Http/Controllers/Api/V1/TicketController.php
+++ app/Http/Controllers/Api/V1/TicketController.php
@@ -18,7 +18,7 @@ class TicketController extends Controller
      */
     public function index()
     {
-        $tickets = Ticket::all();
+        $tickets = Ticket::paginate(10)->get();
         return new TicketCollection($tickets);
     }
 
