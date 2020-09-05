60b616ad290b912f93dd7cbe04d0f22776a2957e
Mrzarabi <m.hadi.z.1997@gmail.com>
2020-09-05 16:56:54 +0430

    add some new filed

--- app/Http/Resources/Api/V1/Product/Product.php
+++ app/Http/Resources/Api/V1/Product/Product.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Resources\Api\V1\Product;
 
+use App\Http\Requests\V1\Images\ImageRequest;
+use App\Http\Resources\Api\V1\Image\ImageCollection;
+use App\Models\Image;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class Product extends JsonResource
@@ -14,16 +17,17 @@ class Product extends JsonResource
      */
     public function toArray($request)
     {
+        // return Image::where('product_id', $this->id)->get();
         return [
             'id'    => $this->id,
             'category' => $this->category ? $this->category->title : '',
             'title' => $this->title,
             'desc' => $this->desc,
             'body' => $this->body,
-            'images' => $this->images,
             'u_price' => $this->u_price,
             'c_price' => $this->c_price,
             'inventory' => $this->inventory,
+            'images' => new ImageCollection(Image::where('product_id', $this->id)->get()),
         ];
     }
 }
