53d720102a0adb0d6a3b8a7600851d101733a92f
Mrzarabi <m.hadi.z.1997@gmail.com>
2020-09-05 11:48:47 +0430

    add method update for user

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\V1\Ticket\TicketRequest;
+use App\Http\Requests\V1\User\UserRequest;
 use App\Http\Resources\Api\V1\User\User as UserResource;
+use App\Models\Ticket;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -11,6 +14,13 @@ use Illuminate\Support\Str;
 
 class UserController extends Controller
 {
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function login(Request $request)
     {
         $validData = $this->validate($request, [
@@ -28,6 +38,13 @@ class UserController extends Controller
         return new UserResource(auth()->user());
     }
 
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function register(Request $request) 
     {
 
@@ -43,5 +60,42 @@ class UserController extends Controller
         ]);
 
         return new UserResource($user);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        return new UserResource($user);
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UserRequest $request, User $user)
+    {
+        if($request->hasFile('avatar')) {
+            $image = $this->upload_image($request->file('avatar'));
+        } else {
+            $image = $user->avatar;
+        }
+
+        $user->update( array_merge($request->all()), [
+            'avatar' => $image
+        ]);
+
+        return response([
+            'data' => 'اطلاعات شما با موفقیت به روز رسانی شد',
+            'status' => 'success'
+        ]);
     }
 }
