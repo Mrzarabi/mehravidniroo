35d0c4dcd031223d18e2fd9ada4a0beb1179946e
Mrzarabi <m.hadi.z.1997@gmail.com>
2020-09-06 21:05:45 +0430

    add field id in colleciton product

--- app/Http/Resources/Api/V1/Product/ProductCollection.php
+++ app/Http/Resources/Api/V1/Product/ProductCollection.php
@@ -19,6 +19,7 @@ class ProductCollection extends ResourceCollection
         return [
             'data' => $this->collection->map( function($item) {
                 return [
+                    'id' => $item->id,
                     'category' => $item->category ? $item->category->title : '',
                     'title' => $item->title,
                     'desc' => $item->desc,
