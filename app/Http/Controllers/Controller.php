c407c3970c95ef4a02644b192d3ada108312ffdb
Mrzarabi <m.hadi.z.1997@gmail.com>
2020-09-08 13:03:37 +0430

    test

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -16,8 +16,7 @@ class Controller extends BaseController
     {
         $time = Carbon::now();
         $file_path = "uploads/images/{$time->year}/{$time->month}/{$time->day}";
-        $file_name = $image->getClientOriginalName();
-        $file_name = $time->timestamp . "-{$file_name}";
+        $file_name = $time->timestamp;
         $image->move(public_path($file_path) , $file_name);
         $file = $file_path . $file_name;
         return $file;
