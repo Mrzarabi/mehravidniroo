0a45ef1a2fdfd2c8d1c22384db87cb2081dd3ece
Mrzarabi <m.hadi.z.1997@gmail.com>
2020-09-09 11:40:12 +0430

    add collection comment

---
+++ app/Http/Resources/Api/V1/Comment/CommentCollection.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources\Api\V1\Comment;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class CommentCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            $this->collection->map( function($item) {
+                return [
+                    'id' => $item->id,
+                    'body' => $item->body,
+                    'children' => new CommentCollection( $item->comments ),
+                    // 'is_show' => $item->is_show,
+                    // 'status' => $item->status 
+                ];
+            })
+        ];
+    }
+}
