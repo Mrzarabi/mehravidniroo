c68552be4bb026956eac43a3c98d088864416122
Mrzarabi <m.hadi.z.1997@gmail.com>
2020-09-09 11:41:07 +0430

    add some new change

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -9,9 +9,8 @@ use Faker\Generator as Faker;
 $factory->define(Product::class, function (Faker $faker) {
     return [
         'title' => $faker->firstName(),
-        'desc' => $faker->paragraph(150),
-        'body' => $faker->paragraph(150),
-        // 'image' => $faker->imageUrl(300, 300),
+        'desc' => $faker->sentence(20),
+        'body' => $faker->sentence(20),
         'u_price' => $faker->numberBetween(10000, 100000),
         'c_price' => $faker->numberBetween(10000, 100000),
         'inventory' => $faker->numberBetween(10000, 100000)
