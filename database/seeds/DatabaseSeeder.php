7976a9193afc9c91b31c9b58bc0cacb44e4215d8
Mrzarabi <m.hadi.z.1997@gmail.com>
2020-09-09 11:41:36 +0430

    add seeder comment

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 use App\Models\Brand;
 use App\Models\Category;
+use App\Models\Comment;
 use App\Models\Product;
 use App\User;
 use Illuminate\Database\Seeder;
@@ -22,11 +23,26 @@ class DatabaseSeeder extends Seeder
             $category->categories()->saveMany( 
                 factory(Category::class, rand(0,5))->make()
              )->each(function($category) use($user) {
-                $category->products()->saveMany(
+                $products = $category->products()->saveMany(
                     factory(Product::class, rand(0,5))
                         ->create(['user_id' => $user->id])
                 );
-             });
+                $products->each(function($product) use($user) {
+                    $comments = $product->comments()->saveMany(
+                        factory(Comment::class, rand(0, 6)) 
+                            ->make( ['user_id' => $user->id])
+                    );
+                    $comments->each( function($comment) use($user, $product) {
+                        $comment->comments()->saveMany( 
+                            factory(Comment::class, rand(0, 6)) 
+                            ->make([
+                                'product_id' => $product->id,
+                                'user_id' => $user->id,
+                                ])
+                        );
+                    });
+                });
+            });
         });
 
         // $categories->each(function($category) use($user){
