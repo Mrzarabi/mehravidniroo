a0a9f7a1e4640cb6720b119c8c8395854a837250
Mrzarabi <m.hadi.z.1997@gmail.com>
2020-09-11 19:19:59 +0430

    add some new change

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -128,6 +128,12 @@ class UserController extends Controller
         ]);
     }
 
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function multiDelete(MultiDeleteUserRequest $request)
     {
         $ids = explode(',', $request->ids);
