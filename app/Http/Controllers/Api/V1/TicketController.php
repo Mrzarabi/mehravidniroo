78b1f464f3082463a0c2b3efd8da2586b5dd297d
Mrzarabi <m.hadi.z.1997@gmail.com>
2020-09-11 19:19:49 +0430

    add method status

--- app/Http/Controllers/Api/V1/TicketController.php
+++ app/Http/Controllers/Api/V1/TicketController.php
@@ -74,4 +74,22 @@ class TicketController extends Controller
             'status' => 'success'
         ]);
     }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function ticketStatus(Ticket $ticket)
+    {
+        $ticket->update([
+            'status' => true
+        ]);
+
+        return response([
+            'data' => "تیکت {$ticket->title} با موفقیت تایید شد",
+            'status' => 'success'
+        ]);
+    }
 }
