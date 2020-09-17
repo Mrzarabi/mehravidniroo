f2d2867e9502f4c17655ebcfb4ea5bda1accc220
Mrzarabi <m.hadi.z.1997@gmail.com>
2020-09-18 01:40:58 +0430

    add role resource

---
+++ app/Http/Resources/Api/V1/Role/Role.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Resources\Api\V1\Role;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Role extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'name' => $this->name
+        ];
+    }
+}
