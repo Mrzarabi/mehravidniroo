8c1f15a154faea3f97734f27744da3f18912fefd
Mrzarabi <m.hadi.z.1997@gmail.com>
2020-09-26 07:46:43 +0330

    add some role

--- app/Http/Resources/Api/V1/Product/Product.php
+++ app/Http/Resources/Api/V1/Product/Product.php
@@ -19,18 +19,55 @@ class Product extends JsonResource
      */
     public function toArray($request)
     {
-        return [
-            'id'    => $this->id,
-            'category' => $this->category ? $this->category->title : '',
-            'category_id' => $this->category->id,
-            'title' => $this->title,
-            'desc' => $this->desc,
-            'body' => $this->body,
-            'u_price' => $this->u_price,
-            'c_price' => $this->c_price,
-            'inventory' => $this->inventory,
-            'images' => new ImageCollection(Image::where('product_id', $this->id)->get()),
-            'comments' => new CommentCollection(Comment::where('product_id', $this->id)->get()),
-        ];
+        if( auth()->user() ) {
+
+            $user = auth()->user();
+            if( $user->hasRole([
+                '3362c127-65aa-4950-b14f-2fc86b53ea88',
+                '100e82ba-e1c0-4153-8633-e1bd228f7399' ])) {
+
+                    return [
+                        'id'    => $this->id,
+                        'category' => $this->category ? $this->category->title : '',
+                        'category_id' => $this->category ? $this->category->id : '',
+                        'title' => $this->title,
+                        'desc' => $this->desc,
+                        'body' => $this->body,
+                        'u_price' => $this->u_price,
+                        'c_price' => $this->c_price,
+                        'inventory' => $this->inventory,
+                        'images' => new ImageCollection(Image::where('product_id', $this->id)->get()),
+                        'comments' => new CommentCollection(Comment::where('product_id', $this->id)->get()),
+                    ];
+
+                } else {
+                    return [
+                        'id'    => $this->id,
+                        'category' => $this->category ? $this->category->title : '',
+                        'category_id' => $this->category ? $this->category->id : '',
+                        'title' => $this->title,
+                        'desc' => $this->desc,
+                        'body' => $this->body,
+                        // 'u_price' => $this->u_price,
+                        'c_price' => $this->c_price,
+                        'inventory' => $this->inventory,
+                        'images' => new ImageCollection(Image::where('product_id', $this->id)->get()),
+                        'comments' => new CommentCollection(Comment::where('product_id', $this->id)->get()),
+                    ];
+                }
+        } else {
+            return [
+                'id'    => $this->id,
+                'category' => $this->category ? $this->category->title : '',
+                'category_id' => $this->category ? $this->category->id : '',
+                'title' => $this->title,
+                'desc' => $this->desc,
+                'body' => $this->body,
+                'c_price' => $this->c_price,
+                'inventory' => $this->inventory,
+                'images' => new ImageCollection(Image::where('product_id', $this->id)->get()),
+                'comments' => new CommentCollection(Comment::where('product_id', $this->id)->get()),
+            ];
+        }
     }
 }
