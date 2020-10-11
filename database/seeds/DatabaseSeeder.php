6051806cd27f6cd801662e2716677401bca628ce
Mrzarabi <m.hadi.z.1997@gmail.com>
2020-10-11 16:15:18 +0330

    delete the extra codes

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,48 +17,48 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $user = factory(User::class)->create();
+        // $user = factory(User::class)->create();
 
-        $categories = factory(Category::class, 10)->create();
-        $categories->each( function($category) use($user) {
-            $category->categories()->saveMany( 
-                factory(Category::class, rand(0,5))->make()
-             )->each(function($category) use($user) {
-                $products = $category->products()->saveMany(
-                    factory(Product::class, rand(0,20))
-                        ->create(['user_id' => $user->id])
-                );
-                $products->each(function($product) use($user) {
-                    $comments = $product->comments()->saveMany(
-                        factory(Comment::class, rand(0, 6)) 
-                            ->make( ['user_id' => $user->id])
-                    );
-                    $comments->each( function($comment) use($user, $product) {
-                        $comment->comments()->saveMany( 
-                            factory(Comment::class, rand(0, 6)) 
-                            ->make([
-                                'product_id' => $product->id,
-                                'user_id' => $user->id,
-                                ])
-                        );
-                    });
-                });
-            });
-        });
+        // $categories = factory(Category::class, 10)->create();
+        // $categories->each( function($category) use($user) {
+        //     $category->categories()->saveMany( 
+        //         factory(Category::class, rand(0,5))->make()
+        //      )->each(function($category) use($user) {
+        //         $products = $category->products()->saveMany(
+        //             factory(Product::class, rand(0,20))
+        //                 ->create(['user_id' => $user->id])
+        //         );
+        //         $products->each(function($product) use($user) {
+        //             $comments = $product->comments()->saveMany(
+        //                 factory(Comment::class, rand(0, 6)) 
+        //                     ->make( ['user_id' => $user->id])
+        //             );
+        //             $comments->each( function($comment) use($user, $product) {
+        //                 $comment->comments()->saveMany( 
+        //                     factory(Comment::class, rand(0, 6)) 
+        //                     ->make([
+        //                         'product_id' => $product->id,
+        //                         'user_id' => $user->id,
+        //                         ])
+        //                 );
+        //             });
+        //         });
+        //     });
+        // });
 
-        $users = factory(User::class, 5)->create();
-        $users->each( function($user) {
-            $tickets = $user->tickets()->saveMany(
-                factory(Ticket::class, rand(3, 5))->make()
-            );
-            $tickets->each( function($ticket) use($user) {
-                $ticket->tickets()->saveMany(
-                    factory(Ticket::class, rand(2, 5))->make(
-                        ['user_id' => $user->id]
-                    )
-                );
-            });
-        });
+        // $users = factory(User::class, 5)->create();
+        // $users->each( function($user) {
+        //     $tickets = $user->tickets()->saveMany(
+        //         factory(Ticket::class, rand(3, 5))->make()
+        //     );
+        //     $tickets->each( function($ticket) use($user) {
+        //         $ticket->tickets()->saveMany(
+        //             factory(Ticket::class, rand(2, 5))->make(
+        //                 ['user_id' => $user->id]
+        //             )
+        //         );
+        //     });
+        // });
         $this->call(LaratrustSeeder::class);
     }
 }
