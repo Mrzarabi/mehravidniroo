6e0699d8fb4dc25bc05c4b539f113310743d3081
Mrzarabi <m.hadi.z.1997@gmail.com>
2020-10-12 13:20:01 +0330

    connect vue to laravel route

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
+Route::get('/{vueroute?}', 'AppController@index')->where('vueroute', '.*');
 
-Route::get('/home', 'HomeController@index')->name('home');
+
