2d7af62a1430b1f6d700aedbd71968d2c12b6df9
Mrzarabi <m.hadi.z.1997@gmail.com>
2020-10-14 00:38:54 +0330

    make persian

---
+++ resources/lang/fa/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'پسورد شما با موفقیت به روز رسانی شد',
+    'sent' => 'لینک بازیابی پسورد به ایمیل شما ارسال شد',
+    'throttled' => 'لطفا صبر کنید',
+    'token' => 'توکن شما معتبر نمی باشد',
+    'user' => "ایمیل وارد شده صحیح نمی باشد",
+
+];
